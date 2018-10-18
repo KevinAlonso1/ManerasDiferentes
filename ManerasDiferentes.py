ffa46b097ab3c9de2b8de39279151a8747777a6b
KevinAlonso1 <37006659+KevinAlonso1@users.noreply.github.com>
2018-10-17 21:59:55 -0500

    El uso de la recursividad.

--- ManerasDiferentes.py
+++ ManerasDiferentes.py
@@ -1,13 +1,23 @@
 __author_='Kevin Alonso'
-
-n=15
-
+r=6
+n=7
+c=0
 def factorial(n):
      while n!=1:
-       return n * factorial(n-1)
+        return n * factorial(n-1)
 
      return 1
+def factorial(r):
+    while r!=1:
+        return r*factorial(r-1)
 
-print('Diferentes maneras de calcular r: ',n)
+    return 1
+c=   (int) (factorial(n)*factorial(r)*factorial(n - r))
+
+print('Diferentes maneras de calcular ')
+print('Numero r','=',r)
+print('Numero n','=',n)
 print()
 print(n,'=',factorial(n))
+print(r,'=',factorial(r))
+print(c)
